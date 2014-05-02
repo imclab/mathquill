f69020a6c8cf82fd25c95c92fc3940c434baa30c
Michael Desa <mjdesa@gmail.com>
2014-05-01 19:06:55 -0700

    Automatic rewrite of CSS class 'mq-large-operator'
    Remaining instances of 'large-operator' (outside test/support/):

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -237,7 +237,7 @@ var SupSub = P(MathCommand, function(_, _super) {
 var SummationNotation = P(MathCommand, function(_, _super) {
   _.init = function(ch, html) {
     var htmlTemplate =
-      '<span class="large-operator non-leaf">'
+      '<span class="mq-large-operator non-leaf">'
     +   '<span class="to"><span>&1</span></span>'
     +   '<big>'+html+'</big>'
     +   '<span class="mq-from"><span>&0</span></span>'
